cd506b43d2c82f5572c7d2ec88f67343e97a944e
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-08-16 22:47:32 +0200

    m4: add getCategoryById method

--- catalogService.js
+++ catalogService.js
@@ -2,4 +2,8 @@ function catalogService (db) {
     this.getProductById = function (id) {
         return db.products.get(id);
     };
+
+    this.getCategoryById = function (id) {
+        return db.categories.get(id);
+    };
 }
