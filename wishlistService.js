205eb6c044472955566f76507bd6761e36aa6604
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2014-08-16 22:36:53 +0200

    f2: add deleteItem method

--- wishlistService.js
+++ wishlistService.js
@@ -2,4 +2,8 @@ function wishlistService (db) {
     this.addItem = function (item) {
         db.wishlistItems.add(item);
     };
+
+    this.deleteItem = function (id) {
+        db.wishlistItems.remove(id);
+    };
 }
